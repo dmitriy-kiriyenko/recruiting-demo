1f62adbae29012c075c269034bd77e076194cd46
Dzmitry Lamanik <dlo@anadeainc.com>
2016-11-11 18:16:54 +0300

    minor changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,20 +4,15 @@ class Question < ApplicationRecord
 
   validates :question, presence: true
 
-  belongs_to :qset, optional: true
- #has_many :qsets
-
   has_many :question_qsets, dependent: :destroy
   has_many :qsets, through: :user_qsets
-
-
 
   validate :must_have_right_answer,  on: [:create , :update]
 
   default_scope { order("questions.created_at DESC") }
 
   def must_have_right_answer
-    errors.add(:base, "must_have_right_answer") if answers.select{|a| a.correct>0 }.empty?
+    errors.add(:base, "must_have_right_answer") if answers.select{|a| a.correct? }.empty?
   end
 
 end
